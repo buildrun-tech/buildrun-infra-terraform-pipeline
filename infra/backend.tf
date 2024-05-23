ff5fa7a1da2d6252a56cc386b3d5eabde1e5a4f2
brunograna <brunogarciarana@gmail.com>
2024-05-23 16:16:56 -0300

    cd: change backend config

--- infra/backend.tf
+++ infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket                  = "buildrun-sa-east-1-terraform-s3-statefile"
     dynamodb_table          = "buildrun-terraform-state-lock"
-#     key                     = "terraform-baseline-project"
+    key                     = "buildrun-infra-terraform-pipeline"
     region                  = "sa-east-1"
 #     profile                 = "terraform"
 #     shared_credentials_file = "~/.aws/credentials"
