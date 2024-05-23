113c7b95f9076da907371ffb8b0a71c6e1339eb6
brunograna <brunogarciarana@gmail.com>
2024-05-23 16:15:51 -0300

    cd: change var location

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,3 +1,10 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket                  = "buildrun-sa-east-1-terraform-s3-statefile"
+    dynamodb_table          = "buildrun-terraform-state-lock"
+#     key                     = "terraform-baseline-project"
+    region                  = "sa-east-1"
+#     profile                 = "terraform"
+#     shared_credentials_file = "~/.aws/credentials"
+  }
 }
