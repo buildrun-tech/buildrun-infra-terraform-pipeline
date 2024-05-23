9ca27304ff897df0949480a96565f7c7ff8d3d9c
brunograna <brunogarciarana@gmail.com>
2024-05-23 16:40:39 -0300

    cd: debug

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket                  = "buildrun-sa-east-1-terraform-s3-statefile"
-    dynamodb_table          = "buildrun-terraform-state-lock"
-    key                     = "buildrun-infra-terraform-pipeline"
-    region                  = "sa-east-1"
+#     bucket                  = "buildrun-sa-east-1-terraform-s3-statefile"
+#     dynamodb_table          = "buildrun-terraform-state-lock"
+#     key                     = "buildrun-infra-terraform-pipeline"
+#     region                  = "sa-east-1"
 #     profile                 = "terraform"
 #     shared_credentials_file = "~/.aws/credentials"
   }
